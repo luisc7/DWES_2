da0fa747173da9b3690a7e4a52704a36808e50d3
Luis Cifuentes <luiscifuentes7@gmail.com>
2021-11-13 16:44:18 +0100

    Primera prueba

---
+++ src/main/java/com/ite/clientes/ControladorGestionClientesApplication.java
@@ -0,0 +1,13 @@
+package com.ite.clientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControladorGestionClientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControladorGestionClientesApplication.class, args);
+	}
+
+}
